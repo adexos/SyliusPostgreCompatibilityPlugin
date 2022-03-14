78f1cf0484fe44a25600b16c0ad7acd298fdcecc
Tony <tsmagghe@adexos.fr>
2022-03-14 23:47:04 +0100

    Prepare 1.10.10

--- README.md
+++ README.md
@@ -7,7 +7,11 @@
 <h1 align="center">PostgreSQL Compatibility Plugin</h1>
 <p align="center">This plugin provide PostgreSQL compatibility for Sylius</p>
 <hr/>
-<h3><center><p>⚠️This is a work in progress plugin</p></center><h3>
+
+⚠️ Version of the module follow sylius versioning, be sure to use the correct tag for you're installed version.    
+We create a new version each time new Sylius release include a new migration.  
+Please read Sylius upgrade guides.
+
 
 ## Documentation
 
@@ -28,4 +32,9 @@ config/packages/adexos_sylius_postgre_compatibility_plugin.yaml
 adexos_sylius_postgre_compatibility_plugin:
   excluded_migration_namespaces:
     - Vendor\Namespace\Migrations
+```
+Then you can re-generate and apply migration for postgre with this command: 
+```php
+bin/console doctrine:migration:diff
+bin/console doctrine:migration:migrate
 ```
---
+++ src/Migrations/Version20220314223756_v1_10_10.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adexos\SyliusPostgreCompatibilityPlugin\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220314223756_v1_10_10 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_order ADD created_by_guest BOOLEAN DEFAULT \'true\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_order DROP created_by_guest');
+    }
+}
